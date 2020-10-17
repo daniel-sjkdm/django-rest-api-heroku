9a283615ea18dce18f7ce48966287029e0d99cb3
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-16 23:19:37 -0500

    Static files and media

--- store/settings.py
+++ store/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+DATABASES = {}
 
 if DEBUG:
     from dotenv import load_dotenv
@@ -140,12 +140,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/"staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_ROOT = BASE_DIR/'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
