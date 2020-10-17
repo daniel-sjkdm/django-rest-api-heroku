b8121b76b723ee0a61a911d4b0012f82d6d2b96c
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-17 01:37:24 -0500

    Remove gis app

--- store/settings.py
+++ store/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
     'items',
     'rest_framework',
     'rest_framework.authtoken',
